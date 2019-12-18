4384ee412ffa3f1810fb6d227d9f84306aaf8414
parksinhyeok <ccy0147@ajoc.ac.kr>
2019-12-18 09:52:20 +0900

    network

--- network/start.sh
+++ network/start.sh
@@ -4,37 +4,39 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Exit on first error, print all commands.
-set -ev
-
-function replacePrivateKey() {
-    echo "ca key file exchange"
-    cp docker-compose-template.yml docker-compose.yml
-    PRIV_KEY=$(ls crypto-config/peerOrganizations/org1.trucker.com/ca/ | grep _sk)
-    sed -i "s/CA_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yml
-}
 
 function checkPrereqs() {
     # check config dir
-    if [ ! -d "crypto-config" ]; then
+    if [! -d "crypto-config"]; then
         echo "crypto-config dir missing"
         exit 1
     fi
+
     # check crypto-config dir
-     if [ ! -d "config" ]; then
+    if [! -d "config"]; then
         echo "config dir missing"
         exit 1
     fi
 }
 
+function replacePrivateKey() {
+    echo "ca key file exchange"
+    PRIV_KEY=$(ls crypto-config/peerOrganizations/artist.example.com/ca/ | grep _sk)
+    sed -i "s/CA_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yml
+}
+
 checkPrereqs
 replacePrivateKey
 
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
 docker-compose -f docker-compose.yml down
 
-replacePrivateKey
-
-docker-compose -f docker-compose.yml up -d 
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.artist.example.com peer0.company.example.com peer0.client.example.com couchdb1 couchdb2 couchdb3 cli
 docker ps -a
 
 # wait for Hyperledger Fabric to start
@@ -43,20 +45,21 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-# 채널 생성
-docker exec cli peer channel create -o orderer.trucker.com:7050 -c trucker -f /etc/hyperledger/configtx/channel.tx
+# Create the channel
+# -e: 환경 변수 지정
+# docker exec -e "CORE_PEER_LOCALMSPID=ArtistMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@artist.example.com/msp" peer0.artist.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 
-# peer0.org1.trucker.com 채널 참여
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.trucker.com/msp" peer0.org1.trucker.com peer channel join -b /etc/hyperledger/configtx/trucker.block
+# Create the channel
+docker exec cli peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+
+# Join peer0.artist.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=ArtistMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@artist.example.com/msp" peer0.artist.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
 sleep 5
 
-# peer0.org2.trucker.com 채널 참여
-docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.trucker.com/msp" peer0.org2.trucker.com peer channel join -b /etc/hyperledger/configtx/trucker.block
+# Join peer0.company.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=CompanyMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@company.example.com/msp" peer0.company.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
 sleep 5
 
-# peer0.org3.trucker.com 채널 참여
-docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org3.trucker.com/msp" peer0.org3.trucker.com peer channel join -b /etc/hyperledger/configtx/trucker.block
+# Join peer0.client.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=ClientMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@client.example.com/msp" peer0.client.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
 sleep 5
-
-docker exec cli peer chaincode install -n cargo -v 1.0 -p github.com/artwork/
-docker exec cli peer chaincode instantiate -v 1.0 -C trucker -n cargo -c '{"Args":["Init"]}' -P 'OR ("Org1MSP.member", "Org2MSP.member")'
